b6a5a45c94c37706af7aa95a5630e1f6b335e1ce
Mamadou Dia <mamadou.dia.6@ulaval.ca>
2017-03-12 18:48:56 -0400

    projet

---
+++ maptest3/maptest3/CustomAnnotation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CoreLocation;
+using MapKit;
+
+namespace maptest3
+{
+	public class CustomAnnotation : MKAnnotation
+	{
+		string title;
+		CLLocationCoordinate2D coord;
+
+		public CustomAnnotation(string title, CLLocationCoordinate2D coord)
+		{
+			this.title = title;
+			this.coord = coord;
+		}
+
+		public override string Title
+		{
+			get
+			{
+				return title;
+			}
+		}
+
+		public override CLLocationCoordinate2D Coordinate
+		{
+			get
+			{
+				return coord;
+			}
+		}
+	}
+}
